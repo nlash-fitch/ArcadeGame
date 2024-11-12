44cd01d7809c6734d1a12d83034604c27ef7605e
nlash-fitch <nlash@student.fitchburgstate.edu>
2024-11-11 23:50:02 -0500

    working on spawning towers

---
+++ Assets/Scripts/TowerSpawn.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerSpawn : MonoBehaviour
+{
+    public float spawnY;
+    public float spawnX;
+    public GameObject[] towers;
+    public int towerCount=1; //how many towers are in the "towers" array
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
