3c5c4df78dfdb1227369006171fcb723f03a69da
Maggie447 <mserrat1@student.fitchburgstate.edu>
2024-11-12 23:38:04 -0500

    attempted to work on arrow spawn

--- Assets/Scripts/ArrowSpawn.cs
+++ Assets/Scripts/ArrowSpawn.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class ArrowSpawn : MonoBehaviour
 {
+    public GameObject arrowPrefab;
+    private GameManager gameManager;
+
+    public float spawnRate = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        SpawnArrow();
+    }
+
+    IEnumerator SpawnArrow()
+    {
+        while (gameManager.isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            Instantiate(arrowPrefab, transform.position, arrowPrefab.transform.rotation);
+        }
     }
 }
