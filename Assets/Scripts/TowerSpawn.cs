479e6e8692bc497287be27e618f40ddf6afbc238
Maggie447 <mserrat1@student.fitchburgstate.edu>
2024-11-13 02:27:24 -0500

    made tower spawn check isGameActive

--- Assets/Scripts/TowerSpawn.cs
+++ Assets/Scripts/TowerSpawn.cs
@@ -10,23 +10,27 @@ public class TowerSpawn : MonoBehaviour
     public int timer;
     public float distance = 20.02971f;
     public float[,] towerAskewXY = { { -1.13f, 6.56f, 8.306f },{ 0.39f, 8.3551f, 10.78f } };
+
+    private GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer++;
-        if (timer >= 600)
+        if (gameManager.isGameActive)
         {
-            int rand = Random.Range(0, towers.Length);
-            Vector3 move = new Vector3(towerAskewXY[0,rand], distance + towerAskewXY[1,rand], 0);
-            distance = distance + Instantiate(towers[rand], move, new Quaternion(0,0,0,0)).GetComponent<BoxCollider>().bounds.size.y;
-            timer = 0;
+            timer++;
+            if (timer >= 600)
+            {
+                int rand = Random.Range(0, towers.Length);
+                Vector3 move = new Vector3(towerAskewXY[0, rand], distance + towerAskewXY[1, rand], 0);
+                distance = distance + Instantiate(towers[rand], move, new Quaternion(0, 0, 0, 0)).GetComponent<BoxCollider>().bounds.size.y;
+                timer = 0;
+            }
         }
-        
     }
 }
